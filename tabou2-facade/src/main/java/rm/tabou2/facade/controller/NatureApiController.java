c91ac559f031775fa974e52039b7ef9932c84734
Anaïs Bringy <anais.Bringy@open-groupe.com>
2020-09-15 14:25:24 +0200

    Suppression des api de création et modification des nature

--- tabou2-facade/src/main/java/rm/tabou2/facade/controller/NatureApiController.java
+++ tabou2-facade/src/main/java/rm/tabou2/facade/controller/NatureApiController.java
@@ -7,7 +7,6 @@ import rm.tabou2.facade.api.NaturesApi;
 import rm.tabou2.service.NatureService;
 import rm.tabou2.service.dto.Nature;
 
-import javax.validation.Valid;
 import java.util.List;
 
 public class NatureApiController implements NaturesApi {
@@ -16,23 +15,9 @@ public class NatureApiController implements NaturesApi {
     private NatureService natureService;
 
     @Override
-    public ResponseEntity<Nature> addNature(@Valid Nature nature) throws Exception {
-
-        return new ResponseEntity<>(natureService.addNature(nature), HttpStatus.OK);
-
-    }
-
-    @Override
-    public ResponseEntity<Nature> editNature(@Valid Nature nature) throws Exception {
-
-        return new ResponseEntity<>(natureService.addNature(nature), HttpStatus.OK);
-
-    }
-
-    @Override
     public ResponseEntity<List<Nature>> getNatures() throws Exception {
         return new ResponseEntity<>(natureService.getAllNatures(true), HttpStatus.OK);
     }
 
-    //TODO : inactivate
+
 }
