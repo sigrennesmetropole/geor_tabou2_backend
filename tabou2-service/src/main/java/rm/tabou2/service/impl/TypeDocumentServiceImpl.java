7863431ccdb28df4976b23af104e99788b19f9c0
YAHYA RIAD <yahya.riad@open-groupe.com>
2020-10-01 11:34:16 +0000

    CDCRM-514: Tabou2-API Rest gestion des documents

--- tabou2-service/src/main/java/rm/tabou2/service/impl/TypeDocumentServiceImpl.java
+++ tabou2-service/src/main/java/rm/tabou2/service/impl/TypeDocumentServiceImpl.java
@@ -2,14 +2,18 @@ package rm.tabou2.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import rm.tabou2.service.TypeDocumentService;
 import rm.tabou2.service.dto.TypeDocument;
 import rm.tabou2.service.exception.AppServiceException;
 import rm.tabou2.service.mapper.TypeDocumentMapper;
 import rm.tabou2.service.utils.PaginationUtils;
+import rm.tabou2.storage.tabou.dao.TypeDocumentCustomDao;
 import rm.tabou2.storage.tabou.dao.TypeDocumentDao;
 import rm.tabou2.storage.tabou.entity.TypeDocumentEntity;
+import rm.tabou2.storage.tabou.item.TypeDocumentCriteria;
 
 import java.util.Date;
 import java.util.List;
@@ -21,6 +25,9 @@ public class TypeDocumentServiceImpl implements TypeDocumentService {
 
     @Autowired
     private TypeDocumentDao typeDocumentDao;
+
+    @Autowired
+    private TypeDocumentCustomDao typeDocumentCustomDao;
 
     @Autowired
     private TypeDocumentMapper typeDocumentMapper;
@@ -53,7 +60,7 @@ public class TypeDocumentServiceImpl implements TypeDocumentService {
             typeDocumentEntity = typeDocumentEntityOpt.get();
         }
 
-        typeDocumentEntity.setDateInactif(typeDocument.getDateInactivite());
+        typeDocumentEntity.setDateInactif(typeDocument.getDateInactif());
 
         typeDocumentEntity.setLibelle(typeDocument.getLibelle());
 
@@ -92,17 +99,11 @@ public class TypeDocumentServiceImpl implements TypeDocumentService {
     }
 
     @Override
-    public List<TypeDocument> searchTypeDocument(String keyword, Integer start, Boolean onlyActive, Integer resultsNumber, String orderBy, Boolean asc)  {
+    public Page<TypeDocument> searchTypeDocument(TypeDocumentCriteria typeDocumentCriteria, Pageable pageable)  {
 
-        List<TypeDocumentEntity> typesDocuments;
+        Page<TypeDocumentEntity> typesDocuments = typeDocumentCustomDao.searchTypeDocument(typeDocumentCriteria, pageable);
 
-        if (Boolean.TRUE.equals(onlyActive)) {
-            typesDocuments = typeDocumentDao.findOnlyActiveByKeyword(keyword, PaginationUtils.buildPageable(start, resultsNumber, orderBy, asc));
-        } else {
-            typesDocuments = typeDocumentDao.findByKeyword(keyword, PaginationUtils.buildPageable(start, resultsNumber, orderBy, asc));
-        }
-
-        return typeDocumentMapper.entitiesToDto(typesDocuments);
+        return typeDocumentMapper.entitiesToDto(typesDocuments,pageable);
 
     }
 
---
+++ tabou2-storage/src/main/java/rm/tabou2/storage/tabou/dao/impl/TypeDocumentCustomDaoImpl.java
@@ -0,0 +1,101 @@
+package rm.tabou2.storage.tabou.dao.impl;
+
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.query.QueryUtils;
+import org.springframework.stereotype.Repository;
+import rm.tabou2.storage.dao.impl.AbstractCustomDaoImpl;
+import rm.tabou2.storage.tabou.dao.TypeDocumentCustomDao;
+import rm.tabou2.storage.tabou.entity.*;
+import rm.tabou2.storage.tabou.item.TypeDocumentCriteria;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class TypeDocumentCustomDaoImpl extends AbstractCustomDaoImpl implements TypeDocumentCustomDao {
+
+    public static final String FIELD_ID= "id";
+    public static final String FIELD_LIBELLE = "libelle";
+    public static final String FIELD_DATE_INACTIF = "dateInactif";
+
+
+    @Qualifier("tabouEntityManager")
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public Page<TypeDocumentEntity> searchTypeDocument(TypeDocumentCriteria typeDocumentCriteria, Pageable pageable) {
+
+
+        List<TypeDocumentEntity> result;
+
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+
+        //Requête pour compter le nombre de résultats total
+        CriteriaQuery<Long> countQuery = builder.createQuery(Long.class);
+        Root<TypeDocumentEntity> countRoot = countQuery.from(TypeDocumentEntity.class);
+        buildQuery(typeDocumentCriteria, builder, countQuery, countRoot);
+        countQuery.select(builder.countDistinct(countRoot));
+        Long totalCount = entityManager.createQuery(countQuery).getSingleResult();
+
+        //Si aucun résultat
+        if (totalCount == 0) {
+            return new PageImpl<>(new ArrayList<>(), pageable, 0);
+        }
+
+        //Requête de recherche
+
+
+        CriteriaQuery<TypeDocumentEntity> searchQuery = builder.createQuery(TypeDocumentEntity.class);
+        Root<TypeDocumentEntity> searchRoot = searchQuery.from(TypeDocumentEntity.class);
+
+        buildQuery(typeDocumentCriteria, builder, searchQuery, searchRoot);
+
+        searchQuery.orderBy(QueryUtils.toOrders(pageable.getSort(),searchRoot,builder));
+
+        TypedQuery<TypeDocumentEntity> typedQuery = entityManager.createQuery(searchQuery);
+
+        List<TypeDocumentEntity> typeDocumentEntities = typedQuery.setFirstResult((int) pageable.getOffset()).setMaxResults(pageable.getPageSize()).getResultList();
+        return new PageImpl<>(typeDocumentEntities, pageable, totalCount.intValue());
+    }
+
+    private void buildQuery(TypeDocumentCriteria typeDocumentCriteria, CriteriaBuilder builder,
+                            CriteriaQuery<?> criteriaQuery, Root<TypeDocumentEntity> searchRoot) {
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if( typeDocumentCriteria.getId() != null) {
+
+            predicates.add(builder.equal(searchRoot.get(FIELD_ID), typeDocumentCriteria.getId()));
+
+        }else{
+
+            if(typeDocumentCriteria.getLibelle() != null){
+
+                predicateStringCriteria(typeDocumentCriteria.getLibelle(), FIELD_LIBELLE, predicates, builder, searchRoot);
+
+            }
+            if(typeDocumentCriteria.getDateInactif() != null){
+                predicates.add(builder.equal(searchRoot.get(FIELD_DATE_INACTIF), typeDocumentCriteria.getDateInactif()));
+            }
+        }
+
+        if (CollectionUtils.isNotEmpty(predicates)) {
+            criteriaQuery.where(builder.and(predicates.toArray(new Predicate[predicates.size()])));
+        }
+
+    }
+}
