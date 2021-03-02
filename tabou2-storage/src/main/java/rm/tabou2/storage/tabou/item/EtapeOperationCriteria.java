ba77f5ef84456236529b8950159d571c88d58e69
NAKOUBAN RAOUL CAMARA <nakouban-raoul.camara@open-groupe.com>
2021-03-02 18:22:46 +0100

    API des recherche des etapes d'operations

---
+++ tabou2-storage/src/main/java/rm/tabou2/storage/tabou/item/EtapeOperationCriteria.java
@@ -0,0 +1,14 @@
+package rm.tabou2.storage.tabou.item;
+
+import lombok.Data;
+
+@Data
+public class EtapeOperationCriteria {
+
+    private String code;
+
+    private String libelle;
+
+    private String mode;
+
+}
