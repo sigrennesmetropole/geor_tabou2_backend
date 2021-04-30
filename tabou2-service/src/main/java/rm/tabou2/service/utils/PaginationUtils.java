0e30d5459ad587c42d4975ffe0e60034fa53e917
Anaïs Bringy <anais.Bringy@open-groupe.com>
2021-04-30 16:33:17 +0200

    CDCRM-759 : paramétrage du nombre de résultats max

--- tabou2-service/src/main/java/rm/tabou2/service/utils/PaginationUtils.java
+++ tabou2-service/src/main/java/rm/tabou2/service/utils/PaginationUtils.java
@@ -1,5 +1,7 @@
 package rm.tabou2.service.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,17 +11,21 @@ import rm.tabou2.service.dto.PageResult;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+@Configuration
 public class PaginationUtils {
 
     private static final Integer DEFAULT_START = 0;
-    private static final Integer DEFAULT_RESULTS_NUMBER = 100;
 
-    /**
-     * Constructeur de la classe utilitaire
-     */
-    private PaginationUtils() {
-        throw new IllegalStateException("Utility class");
+    @Value("${pagination.default.max.results}")
+    private int maxResults;
+
+    private static int maxResultsStatic;
+
+    @Value("${pagination.default.max.results}")
+    public void setMaxResults(int name) {
+        this.maxResultsStatic = name;
     }
+
 
     public static Pageable buildPageable(Integer start, Integer resultsNumber, String orderBy, Boolean asc, Class<?> classname) {
 
@@ -27,7 +33,7 @@ public class PaginationUtils {
             start = DEFAULT_START;
         }
         if (null == resultsNumber) {
-            resultsNumber = DEFAULT_RESULTS_NUMBER;
+            resultsNumber = maxResultsStatic;
         }
 
         Sort.Direction direction = Sort.Direction.ASC;
@@ -48,7 +54,7 @@ public class PaginationUtils {
 
     }
 
-    public static PageResult buildPageResult(Page<?> page){
+    public static PageResult buildPageResult(Page<?> page) {
 
         PageResult result = new PageResult();
 
