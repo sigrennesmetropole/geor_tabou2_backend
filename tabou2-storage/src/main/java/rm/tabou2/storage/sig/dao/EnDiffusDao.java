0e97c9a9078573339893b9520f665463622cef83
Anaïs Bringy <anais.Bringy@open-groupe.com>
2021-08-27 16:23:31 +0200

    CDCRM-843 : Evolution - API liste des emprises OA, modification filtre table OA_limite_intervention

--- tabou2-storage/src/main/java/rm/tabou2/storage/sig/dao/EnDiffusDao.java
+++ tabou2-storage/src/main/java/rm/tabou2/storage/sig/dao/EnDiffusDao.java
@@ -2,6 +2,7 @@ package rm.tabou2.storage.sig.dao;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import rm.tabou2.storage.common.CustomCrudRepository;
 import rm.tabou2.storage.sig.entity.EnDiffusEntity;
 
@@ -9,7 +10,13 @@ import java.util.List;
 
 public interface EnDiffusDao extends CustomCrudRepository<EnDiffusEntity, Integer>, JpaRepository<EnDiffusEntity, Integer> {
 
-    List<EnDiffusEntity> findAllByIdTabouIsNullAndNomIsLikeIgnoreCase(String nom, Pageable pageable);
+
+    @Query("SELECT e FROM EnDiffusEntity e WHERE " +
+            "e.idTabou is NULL AND " +
+            "e.nature is NOT NULL AND " +
+            "e.nature NOT LIKE '' AND " +
+            "UPPER(e.nom) LIKE UPPER(:nom)")
+    List<EnDiffusEntity> findAllByName(String nom, Pageable pageable);
 
     int countAllByIdTabouIsNullAndNomIsLikeIgnoreCase(String nom);
 
