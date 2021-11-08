f56e0e2ebfcb065e1a1683a98f3d1777ec27b973
GMA21293 <gma21293@open-groupe.com>
2021-11-08 11:27:53 +0100

    CDCRM-901 : Ajout vérification droits

--- tabou2-service/src/main/java/rm/tabou2/service/tabou/tiers/impl/TiersServiceImpl.java
+++ tabou2-service/src/main/java/rm/tabou2/service/tabou/tiers/impl/TiersServiceImpl.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import rm.tabou2.service.dto.Tiers;
 import rm.tabou2.service.exception.AppServiceException;
+import rm.tabou2.service.helper.tiers.TiersRightsHelper;
 import rm.tabou2.service.mapper.tabou.tiers.Tiers2Mapper;
 import rm.tabou2.service.mapper.tabou.tiers.TiersMapper;
 import rm.tabou2.service.tabou.tiers.TiersService;
@@ -33,10 +35,16 @@ public class TiersServiceImpl implements TiersService {
     @Autowired
     private TiersCustomDao tiersCustomDao;
 
+    @Autowired
+    private TiersRightsHelper tiersRightsHelper;
+
     @Override
     public Tiers getTiersById(Long tiersId) {
         Optional<TiersEntity> tiersEntityOpt = tiersDao.findById(tiersId);
 
+        if (!tiersRightsHelper.checkCanGetTiers()){
+            throw new AccessDeniedException("L'utilisateur n'a pas les droits d'accéder un tiers");
+        }
         if (tiersEntityOpt.isEmpty()) {
             throw new NoSuchElementException("Le tiers id=" + tiersId + " n'existe pas");
         }
@@ -47,6 +55,9 @@ public class TiersServiceImpl implements TiersService {
     @Override
     public Tiers createTiers(Tiers tiers) {
 
+        if (!tiersRightsHelper.checkCanCreateTiers()){
+            throw new AccessDeniedException("L'utilisateur n'a pas les droits de créer un tiers");
+        }
         TiersEntity tiersEntity = tiersMapper.dtoToEntity(tiers);
 
         tiersEntity = tiersDao.save(tiersEntity);
@@ -58,6 +69,9 @@ public class TiersServiceImpl implements TiersService {
     @Override
     public Tiers updateTiers(Tiers tiers) {
 
+        if(!tiersRightsHelper.checkCanUpdateTiers()){
+            throw new AccessDeniedException("L'utilisateur n'a pas les droits d'éditer un tiers");
+        }
         TiersEntity tiersEntity = tiersMapper.dtoToEntity(tiers);
 
         tiersEntity = tiersDao.save(tiersEntity);
@@ -68,6 +82,9 @@ public class TiersServiceImpl implements TiersService {
 
     @Override
     public Tiers inactivateTiers(long tiersId) throws AppServiceException {
+        if(!tiersRightsHelper.checkCanUpdateTiers()){
+            throw new AccessDeniedException("L'utilisateur n'a pas les droits d'éditer un tiers");
+        }
 
         Optional<TiersEntity> tiersOpt = tiersDao.findById(tiersId);
         if (tiersOpt.isEmpty()) {
@@ -91,7 +108,9 @@ public class TiersServiceImpl implements TiersService {
 
     @Override
     public Page<Tiers> searchTiers(TiersCriteria tiersCriteria, Pageable pageable) {
-
+        if (!tiersRightsHelper.checkCanGetTiers()) {
+            throw new AccessDeniedException("L'utilisateur n'a pas les droits de chercher un tiers");
+        }
         return tiersMapper.entitiesToDto(tiersCustomDao.searchTiers(tiersCriteria, pageable), pageable);
 
     }
@@ -99,7 +118,9 @@ public class TiersServiceImpl implements TiersService {
 
     @Override
     public Page<Tiers> searchTiers2(TiersCriteria tiersCriteria, Pageable pageable) {
-
+        if (!tiersRightsHelper.checkCanGetTiers()) {
+            throw new AccessDeniedException("L'utilisateur n'a pas les droits de chercher un tiers");
+        }
         return tiers2Mapper.entitiesToDto(tiersCustomDao.searchTiers(tiersCriteria, pageable), pageable);
 
     }
