066c1c303fd3d5e923e674c25dfc544ca40d0735
LLE23797 <lisa.le_goff__mauvoisin@open-groupe.com>
2024-03-14 16:00:23 +0100

    CDCRM-1211 mise a jour libelles maitrise d'ouvrage

--- resources/bdd/migration1.3.0Vers1.3.1.sql
+++ resources/bdd/migration1.3.0Vers1.3.1.sql
@@ -1,16 +1,15 @@
+-- Ajout d'attributs dans tabou_consommation_espace
 INSERT INTO tabou_consommation_espace (libelle, code) VALUES ('Coup parti en extension urbaine', 'COUP_PARTI_EXTENSION');
 INSERT INTO tabou_consommation_espace (libelle, code) VALUES ('Coup parti en renouvellement urbain', 'COUP_PARTI_RENOUVLMT');
 
-UPDATE tabou_maitrise_ouvrage SET libelle = 'inter', code = 'inter' WHERE id = 3;
-UPDATE tabou_operation SET id_maitrise_ouvrage = 2 WHERE id_maitrise_ouvrage = 3;
-UPDATE tabou_maitrise_ouvrage SET libelle = 'Intercommunale', code = 'INTERCOMMUNALE' WHERE id = 2;
+-- Mise à jour libelle de tabou_maitrise_ouvrage
+UPDATE tabou_operation SET id_maitrise_ouvrage = 3 WHERE id_maitrise_ouvrage = 2;
+DELETE FROM tabou_maitrise_ouvrage WHERE id = 2;
+INSERT INTO tabou_maitrise_ouvrage (libelle, code) VALUES ('Privé', 'PRIVE');
 
-UPDATE tabou_maitrise_ouvrage SET libelle = 'com', code = 'com' WHERE id = 1;
-UPDATE tabou_operation SET id_maitrise_ouvrage = 3 WHERE id_maitrise_ouvrage = 1;
-UPDATE tabou_maitrise_ouvrage SET libelle = 'Communale', code = 'COMMUNALE' WHERE id = 3;
-
-UPDATE tabou_maitrise_ouvrage SET libelle = 'metro', code = 'metro' WHERE id = 4;
-UPDATE tabou_operation SET id_maitrise_ouvrage = 1 WHERE id_maitrise_ouvrage = 4;
-UPDATE tabou_maitrise_ouvrage SET libelle = 'Métropolitaine', code = 'METROPOLITAINE' WHERE id = 1;
-
-UPDATE tabou_maitrise_ouvrage SET libelle = 'Privé', code = 'PRIVE' WHERE id = 4;
+-- Mise en place d'un ordre pour l'affichage
+ALTER TABLE tabou_maitrise_ouvrage ADD order_ integer;
+UPDATE tabou_maitrise_ouvrage SET order_ = 1 WHERE code = 'METROPOLITAINE';
+UPDATE tabou_maitrise_ouvrage SET order_ = 2 WHERE code = 'INTERCOMMUNALE';
+UPDATE tabou_maitrise_ouvrage SET order_ = 3 WHERE code = 'COMMUNALE';
+UPDATE tabou_maitrise_ouvrage SET order_ = 4 WHERE code = 'PRIVE';
