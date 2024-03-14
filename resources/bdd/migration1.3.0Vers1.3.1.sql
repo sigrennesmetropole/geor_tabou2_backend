a9e84cf3951e7f628189c8255e2f2b48ed050164
LLE23797 <lisa.le_goff__mauvoisin@open-groupe.com>
2024-03-14 11:16:57 +0100

    CDCRM-1189 ajout de nouveaux attributs dans tabou_consommation_espace

---
+++ resources/bdd/migration1.3.0Vers1.3.1.sql
@@ -0,0 +1,2 @@
+INSERT INTO tabou_consommation_espace (libelle, code) VALUES ('Coup parti en extension urbaine', 'COUP_PARTI_EXTENSION');
+INSERT INTO tabou_consommation_espace (libelle, code) VALUES ('Coup parti en renouvellement urbain', 'COUP_PARTI_RENOUVLMT');
