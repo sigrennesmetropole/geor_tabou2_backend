2af9ebb3ebb4c818da62ab3f3cb7af6c497bb02c
LLE23797 <lisa.le_goff__mauvoisin@open-groupe.com>
2024-03-18 14:34:54 +0100

    CDCRM-1211 utilisation des codes au lieu des ids

--- resources/bdd/migration1.3.0Vers1.3.1.sql
+++ resources/bdd/migration1.3.0Vers1.3.1.sql
@@ -3,8 +3,9 @@ INSERT INTO tabou_consommation_espace (libelle, code) VALUES ('Coup parti en ext
 INSERT INTO tabou_consommation_espace (libelle, code) VALUES ('Coup parti en renouvellement urbain', 'COUP_PARTI_RENOUVLMT');
 
 -- Mise à jour libelle de tabou_maitrise_ouvrage
-UPDATE tabou_operation SET id_maitrise_ouvrage = 3 WHERE id_maitrise_ouvrage = 2;
-DELETE FROM tabou_maitrise_ouvrage WHERE id = 2;
+UPDATE tabou_operation SET id_maitrise_ouvrage = (SELECT id FROM tabou_maitrise_ouvrage WHERE code = 'INTERCOMMUNALE')
+                       WHERE id_maitrise_ouvrage = (SELECT id FROM tabou_maitrise_ouvrage WHERE code = 'COMMUNAUTAIRE');
+DELETE FROM tabou_maitrise_ouvrage WHERE code = 'COMMUNAUTAIRE';
 INSERT INTO tabou_maitrise_ouvrage (libelle, code) VALUES ('Privé', 'PRIVE');
 
 -- Mise en place d'un ordre pour l'affichage
