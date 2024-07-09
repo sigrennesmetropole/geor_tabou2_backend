c31eb33455da7014ef7a0d8aae2b2db5cb031ca1
LLE23797 <lisa.le_goff__mauvoisin@open-groupe.com>
2024-07-09 11:52:30 +0200

    CDCRM-1430 Conditionner l'affichage des événements system

--- tabou2-service/src/main/java/rm/tabou2/service/helper/evenement/TypeEvenementRigthsHelper.java
+++ tabou2-service/src/main/java/rm/tabou2/service/helper/evenement/TypeEvenementRigthsHelper.java
@@ -1,17 +1,24 @@
 package rm.tabou2.service.helper.evenement;
 
 import org.springframework.stereotype.Component;
+import rm.tabou2.service.helper.AuthentificationHelper;
 import rm.tabou2.storage.tabou.entity.evenement.TypeEvenementEntity;
 
 @Component
 public class TypeEvenementRigthsHelper {
 
+    private final AuthentificationHelper authentificationHelper;
+
+    public TypeEvenementRigthsHelper(AuthentificationHelper authentificationHelper) {
+        this.authentificationHelper = authentificationHelper;
+    }
+
     /**
-     * Vérifie si l'utilisateur a les droits de récupérer un type d'événement
+     * Vérifie si l'utilisateur a les droits admin de récupérer un type d'événement système
      * @param typeEvenementEntity   type d'événement
      * @return                      true si l'utilisateur peut le récupérer
      */
     public boolean canGetTypeEvenement(TypeEvenementEntity typeEvenementEntity) {
-        return !typeEvenementEntity.isSysteme();
+        return !typeEvenementEntity.isSysteme() || authentificationHelper.hasAdministratorRole();
     }
 }
